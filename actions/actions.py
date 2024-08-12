ef5676e6c4ffff3bf0251fdc3c4d5dca54cc9124
Umesh Chandra Dani <daniumeshchandra.se@gmail.com>
2024-08-12 11:52:48 +0530

    added approval form

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,12 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker, FormValidationAction
+from rasa_sdk.events import EventType
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+import requests
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +27,42 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+ALLOWED_MODULE = ['purchase requisition', 'purchase order', 'non po invoice']
+
+class ValidateApprovalForm(FormValidationAction):
+  def name(self) -> Text:
+    return "validate_approval_form"
+
+  def validate_module(
+      self,
+      slot_value: Any,
+      dispatcher: CollectingDispatcher,
+      tracker: Tracker,
+      domain: DomainDict,
+  ) -> Dict[Text, Any]:
+    if slot_value.lower() not in ALLOWED_MODULE:
+      dispatcher.utter_message(text=f"We only accept purchase requisition, purchase order and non po invoice module.")
+      return {"module": None}
+    dispatcher.utter_message(text=f"OK! You want to approve in {slot_value} module.")
+    return {"module": slot_value}
+  
+class ActionSubmitApprovalForm(Action):
+  def name(self) -> Text:
+    return "action-submit-approval-form"
+  
+  def run(self, dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: DomainDict,) -> List[Dict[Text, Any]]:
+    module = next(tracker.get_latest_entity_values("module"), None)
+    ref_number = next(tracker.get_latest_entity_values("ref_number"), None)
+
+    # if module is None:
+    #   dispatcher.utter_message(text="Module is Invalid, Retry")
+    # if module == ""
+
+    msg = f"Approval Request Sent of module {module} with reference number {ref_number}."
+    dispatcher.utter_message(text=msg)
+
+    return []
+

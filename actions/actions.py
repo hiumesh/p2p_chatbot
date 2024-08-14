24371a9bdd7a47b936be44ec3fc5ffadc722e16b
Umesh Chandra Dani <daniumeshchandra.se@gmail.com>
2024-08-14 11:29:53 +0530

    added rules for edge cases added disapproval and reject forms

--- actions/actions.py
+++ actions/actions.py
@@ -9,7 +9,7 @@
 
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker, FormValidationAction
-from rasa_sdk.events import EventType
+from rasa_sdk.events import EventType, AllSlotsReset
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 import requests
@@ -43,9 +43,9 @@ class ValidateApprovalForm(FormValidationAction):
   ) -> Dict[Text, Any]:
     if slot_value.lower() not in ALLOWED_MODULE:
       dispatcher.utter_message(text=f"We only accept purchase requisition, purchase order and non po invoice module.")
-      return {"module": None}
+      return {"module_name": None}
     dispatcher.utter_message(text=f"OK! You want to approve in {slot_value} module.")
-    return {"module": slot_value}
+    return {"module_name": slot_value}
   
 class ActionSubmitApprovalForm(Action):
   def name(self) -> Text:
@@ -54,15 +54,85 @@ class ActionSubmitApprovalForm(Action):
   def run(self, dispatcher: CollectingDispatcher,
     tracker: Tracker,
     domain: DomainDict,) -> List[Dict[Text, Any]]:
-    module = next(tracker.get_latest_entity_values("module"), None)
-    ref_number = next(tracker.get_latest_entity_values("ref_number"), None)
+    currentValues = tracker.current_slot_values()
 
-    # if module is None:
-    #   dispatcher.utter_message(text="Module is Invalid, Retry")
-    # if module == ""
+    module = currentValues["module_name"] if currentValues["module_name"] else None
+    ref_number = currentValues["ref_number"] if currentValues["ref_number"] else None
 
-    msg = f"Approval Request Sent of module {module} with reference number {ref_number}."
-    dispatcher.utter_message(text=msg)
+    url = 'http://localhost:5000/api/v1/rasa/test'
+    data = {
+      "module_name" : module,
+      "ref_number" : ref_number,
+    }
 
-    return []
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+    dispatcher.utter_message(text=response.json()["message"])
+
+
+    return [AllSlotsReset()]
+  
+class ActionSubmitDisapprovalForm(Action):
+  def name(self) -> Text:
+    return "action-submit-disapproval-form"
+  
+  def run(self, dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: DomainDict,) -> List[Dict[Text, Any]]:
+    currentValues = tracker.current_slot_values()
+
+    module = currentValues["module_name"] if currentValues["module_name"] else None
+    ref_number = currentValues["ref_number"] if currentValues["ref_number"] else None
+    comments = currentValues["comments"] if currentValues["comments"] else None
+
+    url = 'http://localhost:5000/api/v1/rasa/test'
+    data = {
+      "module_name" : module,
+      "ref_number" : ref_number,
+      "comments": comments,
+    }
+
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+    dispatcher.utter_message(text=response.json()["message"])
+
+
+    return [AllSlotsReset()]
+
+class ActionSubmitRejectForm(Action):
+  def name(self) -> Text:
+    return "action-submit-reject-form"
+  
+  def run(self, dispatcher: CollectingDispatcher,
+    tracker: Tracker,
+    domain: DomainDict,) -> List[Dict[Text, Any]]:
+    currentValues = tracker.current_slot_values()
+
+    module = currentValues["module_name"] if currentValues["module_name"] else None
+    ref_number = currentValues["ref_number"] if currentValues["ref_number"] else None
+    comments = currentValues["comments"] if currentValues["comments"] else None
+
+    url = 'http://localhost:5000/api/v1/rasa/test'
+    data = {
+      "module_name" : module,
+      "ref_number" : ref_number,
+      "comments": comments,
+    }
+
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+    dispatcher.utter_message(text=response.json()["message"])
+
+
+    return [AllSlotsReset()]
+
 
